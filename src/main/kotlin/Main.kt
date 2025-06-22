4fb2b80d9a0301cc13d30af57d19f103bb31e8e7
Ali <ahmedalielm02@gmail.com>
2025-06-22 19:56:33 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,56 @@
+fun main() {
+
+    println()
+
+//    val name = "Ali"
+//    println(name.isEmpty())
+//
+//    println(name)
+
+
+//    var price: Long = 1_000_000
+//    println(price + 1_000_000)
+
+
+//    var myName = "Ali ELM"
+//    myName = myName.lowercase()
+//    println(myName)
+
+
+//    var message1 = "Hello,\nMy name is Ali,\nHow are you?"
+//    println(message1)
+
+//    var message2 = """
+//        Hello,
+//        My name is Ali,
+//        How are you?
+//    """.trimIndent()
+//
+//    println(message2)
+
+
+    // Referential Equality
+
+    var a = 12
+    var b = 12
+
+    println(a === b)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
