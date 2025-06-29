ea133c6a5ba205dd5b94fd7bc965e38f83ac9614
Ali <ahmedalielm02@gmail.com>
2025-06-29 14:13:45 +0100

    Initial commit

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,16 +1,34 @@
-class User (var firstName: String, var lastName: String) {
+//class User (var firstName: String, var lastName: String) {
+//
+//    var fullName: String = "$firstName $lastName"
+//        get() = "Full name: $field"
+//
+//    lateinit var favoriteCity: String  // Has to be initialized before use
+//
+//    constructor(firstName: String): this(firstName = firstName, lastName = "") {
+//        println("2nd")
+//    }
+//
+//    fun printFullName() {
+//        println("$firstName  $lastName")
+//    }
+//}
 
-    var fullName: String = "$firstName $lastName"
-        get() = "Full name: $field"
 
-    lateinit var favoriteCity: String  // Has to be initialized before use
 
-    constructor(firstName: String): this(firstName = firstName, lastName = "") {
-        println("2nd")
-    }
 
-    fun printFullName() {
-        println("$firstName  $lastName")
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
