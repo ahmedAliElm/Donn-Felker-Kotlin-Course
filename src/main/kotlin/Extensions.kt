e57e37bbfe05ec8180ab09d183112495e9703288
Ali <ahmedalielm02@gmail.com>
2025-06-30 18:24:19 +0100

    Initial commit

---
+++ src/main/kotlin/Extensions.kt
@@ -0,0 +1,12 @@
+fun String.initials(): String {  // Extension function
+
+    val values = this.split(' ')
+
+    val firstInitial = values[0].substring(0, 1)
+    val lastInitial = values[1].substring(0, 1)
+
+    return "$firstInitial$lastInitial"
+}
+
+fun Int.isAdult(): Boolean = this >= 18
+
--- src/main/kotlin/Main2.kt
+++ src/main/kotlin/Main2.kt
@@ -5,23 +5,52 @@ fun main() {
 
     println()
 
-    measure {
-        val list = generateSequence(1) { it + 1 }
-            .take(50_000_000)
-            .toList()
+//    measure {
+//        val list = generateSequence(1) { it + 1 }
+//            .take(50_000_000)
+//            .toList()
+//
+//        val result = list.filter { it % 3 == 0 }.average()
+//
+//        println("Done")
+//        println(result)
+//    }
 
-        val result = list.filter { it % 3 == 0 }.average()
 
-        println("Done")
-        println(result)
-    }
+    val name = "Ahmed Ali"
+
+    println(name.initials())
 }
 
-fun measure(block: () -> Unit) {
+//fun measure(block: () -> Unit) {
+//
+//    val nanoTime = measureNanoTime(block)
+//    val milliseconds = TimeUnit.NANOSECONDS.toMillis(nanoTime)
+//
+//    println("$milliseconds ms")
+//}
 
-    val nanoTime = measureNanoTime(block)
-    val milliseconds = TimeUnit.NANOSECONDS.toMillis(nanoTime)
 
-    println("$milliseconds ms")
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
